9eef40068ebc1d810dee4e2c25d8170da5d7cb6b
nedol <nedooleg@gmail.com>
2024-05-23 07:52:15 +0200

    fetch chat

--- src/routes/translate/+server.js
+++ src/routes/translate/+server.js
@@ -2,13 +2,16 @@ import { Translate } from './Translate';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ url, fetch, cookies, request }) {
-  let { question} = await request.json();
+  let { question } = await request.json();
 
-  let resp =  await Translate(question.text, question.from_lang, question.to_lang);
+  let resp = await Translate(
+    question.text,
+    question.from_lang,
+    question.to_lang
+  );
 
-    console.log(resp);
-    let response = new Response(JSON.stringify({ resp }));
-    response.headers.append('Access-Control-Allow-Origin', `*`);
-    return response;
-  
+  // console.log(resp);
+  let response = new Response(JSON.stringify({ resp }));
+  response.headers.append('Access-Control-Allow-Origin', `*`);
+  return response;
 }
